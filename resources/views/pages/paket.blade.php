5326bd952dcd9ad0446ab4ddf5e78c2b32db9a44
Shalom_Christviona <shalomchristviona@gmail.com>
2025-07-21 13:55:12 +0700

    update paket layanan

---
+++ resources/views/pages/paket.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.konsumen')
+
+@section('title', 'Dashboard Konsumen')
+
+@section('content')
+
+            <div class="container section-title" data-aos="fade-up">
+                <h2>LAYANAN PAKET</h2>
+                <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+            </div><!-- End Section Title -->
+
+            <div class="container" data-aos="zoom-in" data-aos-delay="100">
+
+                <div class="row g-4">
+
+                    <div class="col-lg-4">
+                        <div class="pricing-item">
+                            <h3>PAKET A</h3>
+                            <div class="icon">
+                                <i class="bi bi-box"></i>
+                            </div>
+                            <h4><sup>Rp</sup>100.000<span> / 7 hari</span></h4>
+                            <ul>
+                                <li><i class="bi bi-check"></i> <span>Media Sosial (1 Platform)</span></li>
+                                <li><i class="bi bi-check"></i> <span>1 Kampanye</span></li>
+                                <li><i class="bi bi-check"></i> <span>Durasi Iklan 7 Hari</span></li>
+                                <li><i class="bi bi-check"></i> <span>Desain Banner</span></li>
+                                <li><i class="bi bi-check"></i> <span>Massa ultricies mi quis hendrerit</span></li>
+                            </ul>
+                            <div class="text-center"><a href="#" class="buy-btn">Baca Selengkapnya</a></div>
+                        </div>
+                    </div><!-- End Pricing Item -->
+
+                    <div class="col-lg-4">
+                        <div class="pricing-item featured">
+                            <h3>PAKET B</h3>
+                            <div class="icon">
+                                <i class="bi bi-rocket"></i>
+                            </div>
+
+                            <h4><sup>Rp</sup>100.000<span> / 7 hari</span></h4>
+                            <ul>
+                                <li><i class="bi bi-check"></i> <span>Media Sosial + Google</span></li>
+                                <li><i class="bi bi-check"></i> <span>3 Kampanye</span></li>
+                                <li><i class="bi bi-check"></i> <span>Durasi Iklan 14 hari</span></li>
+                                <li><i class="bi bi-check"></i> <span>Penjadwalan Iklan</span></li>
+                                <li><i class="bi bi-check"></i> <span>Massa ultricies mi quis hendrerit</span></li>
+                            </ul>
+                            <div class="text-center"><a href="#" class="buy-btn">Baca Selengkapnya</a></div>
+                        </div>
+                    </div><!-- End Pricing Item -->
+
+                    <div class="col-lg-4">
+                        <div class="pricing-item">
+                            <h3>PAKET C</h3>
+                            <div class="icon">
+                                <i class="bi bi-send"></i>
+                            </div>
+                            <h4><sup>Rp</sup>100.000<span> / 7 hari</span></h4>
+                            <ul>
+                                <li><i class="bi bi-check"></i> <span>Media Sosial + Google</span></li>
+                                <li><i class="bi bi-check"></i> <span>6 Kampanye</span></li>
+                                <li><i class="bi bi-check"></i> <span>Durasi Iklan 30 hari</span></li>
+                                <li><i class="bi bi-check"></i> <span>Penjadwalan Iklan</span></li>
+                                <li><i class="bi bi-check"></i> <span>Massa ultricies mi quis hendrerit</span></li>
+                            </ul>
+                            <div class="text-center"><a href="#" class="buy-btn">Baca Selengkapnya</a></div>
+                        </div>
+                    </div><!-- End Pricing Item -->
+
+                </div>
+
+            </div>
+    
+</main>
+@endsection
